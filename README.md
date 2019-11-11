cb9454775ac877571dd2762287ae3493862cf1b3
Tangi <tangui.favennec@gmail.com>
2019-11-11 13:24:41 +0900

    Initial base flyway settings + Users table creation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sample-db-migrations
 Database migration for some other repositories projects
+
+## Migrate
+
+run ```flyway -configFiles=./conf/flyway.conf -X migrate```
---
+++ sql/V1__Create_Users_Table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    id SERIAL NOT NULL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email_address VARCHAR(100) NOT NULL UNIQUE
+);
